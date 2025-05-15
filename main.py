dacd2c6528c029a0766de286404a4e7eff6b8356
Jonah Whaler <115598785+JonahWhaler@users.noreply.github.com>
2025-05-15 09:31:44 +0800

    Add code example.

--- main.py
+++ main.py
@@ -1,57 +1,50 @@
-import os
+# import os
+import json
 import logging
 from dotenv import load_dotenv
-import pandas as pd
-from ragic import DataClient, QueryFilterType
+
+# import pandas as pd
+from ragic import (
+    RagicAPIClient,
+    OperandType,
+    Ordering,
+    OrderingType,
+    OtherGETParameters,
+)
 
 
 if __name__ == "__main__":
     load_dotenv()
     logging.basicConfig(
-        filename="./logs/ragic.log",
+        filename=r"./logs/ragic.log",
         level=logging.INFO,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
     logger = logging.getLogger(__name__)
-    client = DataClient(
-        base_url=os.environ.get("RAGIC_URL"),
-        namespace=os.environ.get("RAGIC_NAMESPACE"),
-        api_key=os.environ.get("RAGIC_API_KEY"),
+    client = RagicAPIClient(
+        base_url=None,
+        namespace=None,
+        api_key=None,
         version=3,
-        structure_path="./config/structure.yaml",
+        structure_path="structure.yaml",
     )
-    # Parameters
-    TAB_NAME = os.environ.get("TARGET_TAB")
-    TABLE_NAME = os.environ.get("TARGET_TABLE")
 
-    assert TAB_NAME is not None and TABLE_NAME is not None
+    TAB_NAME = "PB"
+    TABLE_NAME = "Donations"
+    # [("Race", OperandType.EQUALS, "Others")]
+    data_dict = client.load(
+        TAB_NAME,
+        TABLE_NAME,
+        conditions=None,
+        offset=0,
+        size=10,
+        other_get_params=OtherGETParameters(subtables=False, listing=False),
+        ordering=Ordering(order_by="ID", order=OrderingType.ASC),
+    )
 
-    conditions = [
-        # ("FIELD 1", QueryFilterType.CONTAINS, "FIELD VALUE 1"),
-        # ("FIELD 2", QueryFilterType.EQUAL, "FIELD VALUE 2"),
-    ]
-    offset = 0
-    LIMIT = 1000
-    collected_rows = 0
-    main_df = pd.DataFrame()
-    try:
-        while True:
-            # time.sleep(0.2)  # Prevent Rate Limit
-            client.define_query(
-                tab_name=TAB_NAME, table_name=TABLE_NAME, conditions=conditions
-            )
-            df = client.get_dataframe(offset=offset, limit=LIMIT)
-            if df is not None and df.shape[0] > 0:
-                main_df = pd.concat([main_df, df], axis=0, ignore_index=True)
-                # df.info()
-                collected_rows += df.shape[0]
-                logger.info("Total rows collected: %s", collected_rows)
-                offset += LIMIT
-            else:
-                break
-        main_df.info()
-        main_df.to_csv("./output/output.csv", index=False)
-    except Exception as e:
-        logger.error("Main loop error: %s", e)
-        logger.warning("Offset: %s", offset)
+    if data_dict:
+        with open(r"./output/data.json", "w", encoding="utf-8") as f:
+            f.write(json.dumps(data_dict, ensure_ascii=False, indent=4))
+    else:
+        logger.info("No data found.")
